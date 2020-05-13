763c22c41215948e1afb3c7be6282109f10c4461
abi <brawlershere@gmail.com>
2020-05-13 23:05:51 +0545

    patches

--- corona_v2.py
+++ corona_v2.py
@@ -1,37 +1,46 @@
 # Required python modules
 import requests
 from bs4 import BeautifulSoup
-
+import sys
 
 # Global Variables
-j = 0
+choice = 0
 
 
 # gives numbers based on given link
 def stats(link):
     i = 0
-    ok = ["Total number of cases: ", "Deaths: ", "Recovred: "]
+    ok = ["Total number of cases: ", "Deaths: ", "Recovered: "]
     statistics = ""
     result = requests.get(link).content
     soup = BeautifulSoup(result, "html.parser")
     numbers = soup.findAll(id, "maincounter-number")
-    while i < 3:
-        statistics = statistics + (ok[i] + numbers[i].text.strip() + "\n")
-        i += 1
-    return statistics
-
+    try:
+        while i < 3:
+            statistics = statistics + (ok[i] + numbers[i].text.strip() + "\n")
+            i += 1
+        return statistics
+    except IndexError:
+        sys.exit("Invalid Input!\nPLease try again!\n")
 
 # Determines the required link on the basis of user input
-while j == 0:
-    j = 1
+while choice == 0:
     choice = input("Which stats do you want to see?\n1)World\n2)Country\n")
     if choice == "1":
         url = "http://www.worldometers.info/coronavirus/"
     elif choice == "2":
         country = input("Enter Country: ").lower()
+        if country == "usa":
+            country == "us"
+        if country =="uae":
+            country = "united-arab-emirates"
+        country = country.replace(" ","-")
         url = "http://www.worldometers.info/coronavirus/country/" + country
-    else:
-        j = 0
+    elif choice != "1" and choice != "2":
+        print("Invalid Input!!\nPlease try again!\n")
+        choice = 0
 
-
-print("\n" + stats(url))
+try:
+    print("\n" + stats(url))
+except TypeError:
+    sys.exit("Invalid Input!\nPLease try again!\n")
